32eadcb5ed2f6665c1893c2c6afa4dbf71819bb7
brynpirie <brynpirie@gmail.com>
2018-04-28 03:38:09 +0100

    Began work on Neo4J Connector
    Oh boy

---
+++ src/java/Connector/Connector.java
@@ -0,0 +1,37 @@
+package java.Connector ;
+
+import org.neo4j.jdbc.Connection;
+import org.neo4j.jdbc.ResultSet;
+import org.neo4j.jdbc.BaseDriver;
+import org.neo4j.jdbc.PreparedStatement;
+
+public class Connector {
+
+    private final Driver driver;
+
+    public static void initConn() {
+
+        // Connecting
+
+        try (Connection con = DriverManager.getConnection("jdbc:neo4j:bolt://localhost:7687", 'neo4j', password))
+        {
+            System.out.println("Attempting to initiate Neo4J connection...")
+            // * Query
+            String query = "MATCH (n) RETURN (n)";
+
+            try (PreparedStatement stmt = con.prepareStatement(query))
+            {
+
+                try (ResultSet rs = stmt.execute())
+                {
+                    while (rs.next())
+                    {
+                        System.out.println(rs.getString("f.name")+" is "+rs.getInt("f.age"));
+                    }
+                }
+            }
+        }
+
+    } //END initConn()
+
+} //END Connector
---
+++ src/java/main.java
@@ -0,0 +1,16 @@
+package java;
+
+import java.Connector.Connector;
+import spark.Spark;
+
+
+public class Main {
+
+    public static void Main() {
+
+        System.out.println("Attempting to run initConn...");
+        Connector.initConn();
+
+    } //END Main()
+
+} //END Main
