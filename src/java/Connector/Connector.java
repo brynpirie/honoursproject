966bb38acbe84a87571483d8ad2a806bcbfa082d
brynpirie <brynpirie@gmail.com>
2018-05-02 18:58:34 +0100

    yikes
    jdbc and neo4j are not friends right now

--- src/java/Connector/Connector.java
+++ src/java/Connector/Connector.java
@@ -1,51 +1,66 @@
 package Connector ;
 
-import org.neo4j.jdbc.Connection;
-import org.neo4j.jdbc.ResultSet;
-import org.neo4j.jdbc.PreparedStatement;
-import java.sql.DriverManager;
+import org.neo4j.driver.v1.*;
+import java.sql.*;
+
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.jdbc.*;
+import org.neo4j.*;
+import java.util.*;
+
+//import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Connector {
 
-    private final Driver driver;
-    private String password;
+    Driver driver = GraphDatabase.driver(
+            "bolt://localhost", AuthTokens.basic( "neo4j", "neo4j")
+    );
+    Session session = driver.session();
+
+    session.run( "CREATE (a:Person {name:'Arthur', title:'King;})");
+
+    StatementResult result = session.run(
+            "MATCH (a:Person)" +
+                    "WHERE a.name = 'Arthur'" +
+                    "RETURN a.name AS name, a.title AS title"
+    );
+    while (result.hasNext() ){
+        Record record = result.next();
+        System.out.println( record.get( "title").asString() + " " + record.get("name").asString() );
+    }
+
+    session.close();
+    driver.close();
+
+    /**private String password;
 
     {
         password = "neo4j";
     }
 
-    public static Connection getConnection(String url, Properties info) {
-
-        url = "";
-        //info = ;
-    return
-    }
-
     public static void initConn() {
 
-        // Connecting
+        try {
+            // Connecting
+            try (Connection con = DriverManager.getConnection("jdbc:neo4j:bolt://localhost:7687", "neo4j", "neo4j")) {
+                System.out.println("Attempting to initiate Neo4J connection...");
+                // * Query
+                String query = "MATCH (n) RETURN (n)";
 
-        try (Connection con = DriverManager.getConnection("jdbc:neo4j:bolt://localhost:7687", "neo4j", "neo4j"))
-        {
-            System.out.println("Attempting to initiate Neo4J connection...");
-            // * Query
-            String query = "MATCH (n) RETURN (n)";
+                try (PreparedStatement stmt = con.prepareStatement(query)) {
 
-            try (PreparedStatement stmt = con.prepareStatement(query))
-            {
-
-                try (ResultSet rs = stmt.execute())
-                {
-                    while (rs.next())
-                    {
-                        System.out.println(rs.getString("f.name")+" is "+rs.getInt("f.age"));
+                    try (ResultSet rs = stmt.execute()) {
+                        while (rs.next()) {
+                            System.out.println(rs.getString("f.name") + " is " + rs.getInt("f.age"));
+                        }
                     }
                 }
-            }
-        } catch(Exception e){
-            
-        }
+            } catch (Exception e) {
+                SQLException.getMessage();
 
-    } //END initConn()
+            }
+        }
+    } //END initConn() **/
 
 } //END Connector
--- src/java/Main.java
+++ src/java/Main.java
@@ -1,5 +1,4 @@
-
-import java.Connector.Connector;
+import org.neo4j.driver.v1.*;
 import spark.Spark;
 
 
@@ -7,8 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Attempting to run initConn...");
-        Connector.initConn();
+        Driver driver = GraphDatabase.driver(
+                "bolt://localhost", AuthTokens.basic( "neo4j", "neo4j")
+        );
+        Session session = driver.session();
+
+        session.run( "CREATE (a:Person {name:'Arthur', title:'King;})");
+
+        StatementResult result = session.run(
+                "MATCH (a:Person)" +
+                        "WHERE a.name = 'Arthur'" +
+                        "RETURN a.name AS name, a.title AS title"
+        );
+        while (result.hasNext() ){
+            Record record = result.next();
+            System.out.println( record.get( "title").asString() + " " + record.get("name").asString() );
+        }
+
+        session.close();
+        driver.close();
 
     } //END Main()
 
